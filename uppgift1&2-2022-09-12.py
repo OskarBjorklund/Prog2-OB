21597ee2232da20bd55730de670f1ea6ef72cd33
OskarBjorklund <92572944+OskarBjorklund@users.noreply.github.com>
2022-09-12 14:34:47 +0200

    Add files via upload

---
+++ uppgift1&2-2022-09-12.py
@@ -0,0 +1,15 @@
+class Elev:
+    def __init__(self, namn, ålder, godkänd):
+        self.namn = namn
+        self. ålder = ålder
+        self. godkänd = godkänd
+    
+    def __str__(self):
+        if self.godkänd == True:
+            return(f"Hej jag heter {self.namn} och är {self.ålder} och just nu är jag glad för jag fick godkänt")
+        else:
+            return(f"Hej jag heter {self.namn} och är {self.ålder} och just nu är jag ledsen för jag inte fick godkänt")
+
+Axel = Elev("Axel Brandel", "18", True)
+
+print(Axel)
