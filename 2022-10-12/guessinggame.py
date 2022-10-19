4e2bdff98983f9bc466975649423e4932e97f4a4
OskarBjorklund <92572944+OskarBjorklund@users.noreply.github.com>
2022-10-19 10:53:46 +0200

    guessinggame
    inte klar, en liten bugg

--- 2022-10-12/guessinggame.py
+++ 2022-10-12/guessinggame.py
@@ -1,11 +1,20 @@
-def liercheck(guesses):
-    truth = True
-    if guesses[1] == "too high":
-        top = guesses[0]
-    elif guesses[1] == "too low":
-        bottom = guesses[0]
+def liarcheck(guesses):
 
-    print(bottom)
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for i in range(0, len(guesses), 2):
+        if guesses[i+1] == "too high":
+            for j in range(int(guesses[i])-1, len(numbers)-1):
+                numbers[j] = 0
+            numbers[int(guesses[i])-1:] 
+        elif guesses[i+1] == "too low":
+            for j in range(int(guesses[i])):
+                numbers[j] = 0
+        elif guesses[i+1] == "right on":
+            if int(guesses[i]) in numbers:
+                print("Stan may be honest")
+            else:
+                print("Stan is dishonest")
+            numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 inputs = []
 while True:
@@ -14,5 +23,5 @@ while True:
         break
     else:
         inputs.append(adder)
-
-liercheck(inputs)
+        
+liarcheck(inputs)
