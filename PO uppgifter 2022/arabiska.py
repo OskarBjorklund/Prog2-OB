ebe5589d4503cc7b3bf6ae6d266b02a9f1c0bd8a
OskarBjorklund <92572944+OskarBjorklund@users.noreply.github.com>
2022-11-09 09:52:17 +0100

    arabiska
    po uppgift

--- PO uppgifter 2022/arabiska.py
+++ PO uppgifter 2022/arabiska.py
@@ -1,11 +1,17 @@
-allowed = ["a", "e", "i", "o", "u", "y", " ", "."]
+allowed1 = ["a", "e", "i", "o", "u", "y"]
+allowed2 = ["a", "e", "i", "o", "u", "y", " ", "."]
 
 def omvandare(mening):
     lista = list(mening)
     omvand = []
+    antal_pops = 0
     for i in range(len(lista)-2):
-        if lista[i] in allowed:
-                if lista[i+1] and lista[i+2] not in allowed:
+        if lista[i] in allowed1:
+                if lista[i+1] not in allowed2 and lista[i+2] not in allowed2:
+                    antal_pops += 1
+    for i in range(len(lista)-2-antal_pops):
+        if lista[i] in allowed1:
+                if lista[i+1] not in allowed2 and lista[i+2] not in allowed2:
                     lista.pop(i)
     
     for i in range(len(lista)):
