7dbb47a2f873a93d1a0387919177059dfee6c7fb
OskarBjorklund <92572944+OskarBjorklund@users.noreply.github.com>
2023-02-01 10:52:50 +0100

    socket
    socket

--- network/server.py
+++ network/server.py
@@ -7,16 +7,33 @@ PORT = 8822
 
 class Server:
     def __init__(self) -> None:
+        print("Server is starting...")
         self.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
         self.socket.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
 
         self.socket.bind((IP, PORT))
         self.socket.listen()
+        print(f"Server listening on {IP}:{PORT}")
+
+
     
     def accept_clients(self):
         client, address = self.socket.accept()
         thread = threading.Thread(target=self.client_handler, args=(client, address))
         thread.start()
+        print(f"Active connections: {threading.activeCount() - 1}")
 
-    def client_handler(self):
-        print("Bing chilling")
+    def client_handler(self, client, address):
+        print(f"New client connected: {address}")
+        
+        connected = True
+        while connected:
+            msg = client.recv(1024).decode("utf-8")
+            if msg == "/disconnect":
+                connected = False
+            else:
+                print(f"{address}: {msg}")
+                msg = f"Message recieved: {msg}"
+                client.send(msg.encode("utf-8"))
+            
+        client.close()
