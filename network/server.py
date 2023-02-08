100f4ef201a85bc08512f6ed42894cb637bac44d
OskarBjorklund <92572944+OskarBjorklund@users.noreply.github.com>
2023-02-08 10:50:40 +0100

    server
    hmm funkar inte

--- network/server.py
+++ network/server.py
@@ -6,21 +6,6 @@ IP = "localhost"
 PORT = 8822
 
 class Server:
-    def __init__(self) -> None:
-        print("Server is starting...")
-        self.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-        self.socket.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
-
-        self.socket.bind((IP, PORT))
-        self.socket.listen()
-        print(f"Server listening on {IP}:{PORT}")   
-
-    def accept_clients(self):
-        client, address = self.socket.accept()
-        thread = threading.Thread(target=self.client_handler, args=(client, address))
-        thread.start()
-        print(f"Active connections: {threading.activeCount() - 1}")
-
     def client_handler(self, client, address):
         print(f"New client connected: {address}")
         
@@ -35,5 +20,19 @@ class Server:
             client.send(msg.encode("utf-16"))
             
         client.close()
+    
+    def __init__(self) -> None:
+        print("Server is starting...")
+        self.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        self.socket.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
+
+        self.socket.bind((IP, PORT))
+        print(f"Server listening on {IP}:{PORT}")   
+
+        while True:
+            client, address = self.socket.accept()
+            thread = threading.Thread(target=self.client_handler, args=(client, address))
+            thread.start()
+            print(f"Active connections: {threading.activeCount() - 1}")
 
 server = Server()
