cd1d7e6c32d6fc044592c1d3a09e09048e9581fe
OskarBjorklund <92572944+OskarBjorklund@users.noreply.github.com>
2023-03-27 15:25:00 +0200

    Tkinter integrerad
    Integrerade tkinter så att man kan skicka information till databasen.

--- databaser/testdatabas.py
+++ databaser/testdatabas.py
@@ -7,41 +7,52 @@ mydb = mysql.connector.connect(
   password="",  # dito lösenord (en tom sträng)
   database="co"  # byt namn om din databas heter något annat
 )
-mycursor = mydb.cursor()
-print("Uppkopplad till databasen!")
-
-# Skriva till databasen
-sql = "INSERT INTO users (name, address, city, country) VALUES (%s, %s, %s, %s)"
-val = ("Karl Rosengren", "Slånbärsstigen 19", "Åkersberga", "Sweden")
-#mycursor.execute(sql, val)
-#mydb.commit()
-print(mycursor.rowcount, "record inserted.")
-
-# Läsa från databasen
-mycursor.execute("SELECT * FROM users")
-myresult = mycursor.fetchall()
-for x in myresult:
-  print(x)
 
 class Gui:
   def __init__(self):
-    self.window = tk.Tk()
 
-    self.textbox1 = tk.Text(self.window, font=("Arial", 16))
-    self.textbox1.grid(row=0, column=1)
+    self.root = tk.Tk()
 
-    self.textbox2 = tk.Text(self.window, font=("Arial", 16))
-    self.textbox2.grid(row=1, column=1)
+    self.l1 = tk.Label(self.root, text = "Name:")
+    self.l2 = tk.Label(self.root, text = "Address:")
+    self.l3 = tk.Label(self.root, text = "City:")
+    self.l4 = tk.Label(self.root, text = "Country:")
 
-    self.textbox3 = tk.Text(self.window, font=("Arial", 16))
-    self.textbox3.grid(row=2, column=1)
+    self.l1.grid(row = 0, column = 0, sticky = "W", pady = 2)
+    self.l2.grid(row = 1, column = 0, sticky = "W", pady = 2)
+    self.l3.grid(row = 2, column = 0, sticky = "W", pady = 2)
+    self.l4.grid(row = 3, column = 0, sticky = "W", pady = 2)
 
-    self.textbox4 = tk.Text(self.window, font=("Arial", 16))
-    self.textbox4.grid(row=3, column=1)
+    self.e1 = tk.Entry(self.root)
+    self.e2 = tk.Entry(self.root)
+    self.e3 = tk.Entry(self.root)
+    self.e4 = tk.Entry(self.root)
 
-    self.textbox5 = tk.Text(self.window, font=("Arial", 16))
-    self.textbox5.grid(row=4, column=1)
+    self.e1.grid(row = 0, column = 1, pady = 2)
+    self.e2.grid(row = 1, column = 1, pady = 2)
+    self.e3.grid(row = 2, column = 1, pady = 2)
+    self.e4.grid(row = 3, column = 1, pady = 2)
 
-    self.window.mainloop()
+    self.bt = tk.Button(self.root, text = "Confirm", command = self.send_info)
+    
+    self.bt.grid(row = 4, column = 0, columnspan = 2)
+
+    self.root.mainloop()
+
+  def send_info(self):
+    mycursor = mydb.cursor()
+    print("Uppkopplad till databasen!")
+    sql = "INSERT INTO users (name, address, city, country) VALUES (%s, %s, %s, %s)"
+    val = (self.e1.get(), self.e2.get(), self.e3.get(), self.e4.get())
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
+    # Läsa från databasen
+    mycursor.execute("SELECT * FROM users")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+      print(x)
+
 
 gui = Gui()
