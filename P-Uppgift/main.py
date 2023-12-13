dc4ee8f1b778ed5fc35d81208e175ce3290f7c9e
Oskar Björklund <75362016+Odderoon@users.noreply.github.com>
2023-12-13 15:03:51 +0100

    crud
    crudinton

--- P-Uppgift/main.py
+++ P-Uppgift/main.py
@@ -43,8 +43,13 @@ class StartGui:
         self.search_registry(text)
 
     def search_registry(self, registry_name):
-        self.root.destroy()
-        self.registry = open_file(registry_name)
+        # Check if the file exists before destroying the root
+        file_path = os.path.join(os.path.dirname(__file__), registry_name)
+        if os.path.exists(file_path):
+            self.root.destroy()
+            self.registry = open_file(registry_name)
+        else:
+            messagebox.showerror("Error", f"File '{registry_name}' not found in the current directory.")
 
 
     def on_closing(self):
@@ -78,12 +83,12 @@ class RegistryGui:
             self.list_tree.insert("", "end", values = i)
             self.list_tree.bind("<Double-1>", self.double_click)
     
-    def clear_tree(self, tree):
+    def clear_tree(self):
         """
-        Removes all children of specific tree?
+        Removes all children from the tree
         """
-        for item in tree.get_children():
-            tree.delete(item)
+        for item in self.list_tree.get_children():
+            self.list_tree.delete(item)
 
     def double_click(self, event):
         """
@@ -92,6 +97,30 @@ class RegistryGui:
         item = self.list_tree.selection()
         for i in item:
             print(self.list_tree.item(i, "values")[0])
+
+    def add_person(self):
+        # Retrieve the data from entry widgets
+        first_name = self.first_name_entry.get()
+        last_name = self.last_name_entry.get()
+        phone = self.phone_entry.get()
+        email = self.email_entry.get()
+        address = self.address_entry.get()
+
+        # Create a new Person object
+        new_person = Person(last_name, first_name, phone, email, address)
+
+        # Add the new person to the person list
+        self.person_list.append(new_person)
+
+        # Optionally clear the entry fields
+        self.first_name_entry.delete(0, tk.END)
+        self.last_name_entry.delete(0, tk.END)
+        self.phone_entry.delete(0, tk.END)
+        self.email_entry.delete(0, tk.END)
+        self.address_entry.delete(0, tk.END)
+
+        self.clear_tree()
+        self.insert_tree(self.to_nested_list())
     
     def register_gui(self):
         self.wrapper = tk.LabelFrame(self.root)
@@ -100,25 +129,51 @@ class RegistryGui:
         self.wrapper.pack(fill="both", expand="yes", padx=10, pady=10)
         self.list_frame.pack(fill="both", expand="yes", padx=10, pady=10)
 
+        self.first_name_entry = tk.Entry(self.wrapper, font = FONT)
+        self.first_name_label = tk.Label(self.wrapper, text = "Förnamn", font = FONT)
+        self.last_name_entry = tk.Entry(self.wrapper, font = FONT)
+        self.last_name_label = tk.Label(self.wrapper, text = "Efternamn", font = FONT)
+        self.phone_entry = tk.Entry(self.wrapper, font = FONT)
+        self.phone_label = tk.Label(self.wrapper, text = "Telefonnummer", font = FONT)
+        self.email_entry = tk.Entry(self.wrapper, font = FONT)
+        self.email_label = tk.Label(self.wrapper, text = "E-post", font = FONT)
+        self.address_entry = tk.Entry(self.wrapper, font = FONT)
+        self.address_label = tk.Label(self.wrapper, text = "Adress", font = FONT)
+
+        self.add_person_button = tk.Button(self.wrapper, text = "Lägg till person", font = FONT, command = self.add_person)
+
+        self.first_name_entry.grid(row = 1, column = 0, pady = 5, padx = 5)
+        self.first_name_label.grid(row = 0, column = 0, pady = 5, padx = 5)
+        self.last_name_entry.grid(row = 1, column = 1, pady = 5, padx = 5)
+        self.last_name_label.grid(row = 0, column = 1, pady = 5, padx = 5)
+        self.phone_entry.grid(row = 1, column = 2, pady = 5, padx = 5)
+        self.phone_label.grid(row = 0, column = 2, pady = 5, padx = 5)
+        self.email_entry.grid(row = 1, column = 3, pady = 5, padx = 5)
+        self.email_label.grid(row = 0, column = 3, pady = 5, padx = 5)
+        self.address_entry.grid(row = 1, column = 4, pady = 5, padx = 5)
+        self.address_label.grid(row = 0, column = 4, pady = 5, padx = 5)
+
+        self.add_person_button.grid(row = 2, column = 2, pady = 5, padx = 5)
+
         self.list_frame.pack_propagate(0) 
 
         self.list_tree = ttk.Treeview(self.list_frame)
-        self.list_tree["columns"] = ("first_name", "last_name", "phone", "email", "address")
+        self.list_tree["columns"] = ("last_name", "first_name", "phone", "email", "address")
 
         self.list_tree.column("# 0", width=0, stretch=tk.NO)  # Set width to 0 to make it invisible
-        self.list_tree.column("first_name", width=150)
         self.list_tree.column("last_name", width=150)
+        self.list_tree.column("first_name", width=150)
         self.list_tree.column("phone", width=150)
         self.list_tree.column("email", width=150)
         self.list_tree.column("address", width=150)
 
-        self.list_tree.heading("first_name", text="Förnamn")
         self.list_tree.heading("last_name", text="Efternamn")
+        self.list_tree.heading("first_name", text="Förnamn")
         self.list_tree.heading("phone", text="Telefonnummer")
         self.list_tree.heading("email", text="E-post")
         self.list_tree.heading("address", text="Adress")
 
-        self.list_tree["displaycolumns"] = ("first_name", "last_name", "phone", "email", "address")
+        self.list_tree["displaycolumns"] = ("last_name", "first_name", "phone", "email", "address")
 
         style = ttk.Style()
         style.configure("Treeview", fieldbackground="white")
@@ -161,16 +216,13 @@ def create_person_from_line(line):
 
 def open_file(filename):
     file_path = os.path.join(os.path.dirname(__file__), filename)
-    if os.path.exists(file_path):
-        person_list = []
-        with open(file_path, 'r', encoding='utf-8') as file:  # Specify UTF-8 encoding
-            for line in file:
-                person = create_person_from_line(line)
-                person_list.append(person)
-            registry = RegistryGui(filename, person_list)  # Creating RegistryGUI instance with person_list
-            return registry
-    else:
-        messagebox.showerror("Error", f"File '{filename}' not found in the current directory.")
+    person_list = []
+    with open(file_path, 'r', encoding='utf-8') as file:  # Specify UTF-8 encoding
+        for line in file:
+            person = create_person_from_line(line)
+            person_list.append(person)
+        registry = RegistryGui(filename, person_list)  # Creating RegistryGUI instance with person_list
+        return registry
 
 
 StartGui()
